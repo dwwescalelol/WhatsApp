b1b9e032e770774e182a66cd69a0cfa88a818e31
Ezzat Abdallah <20049524@stu.mmu.ac.uk>
2023-03-03 00:49:59 +0000

    Input Box

---
+++ src/components/InputBox.js
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { View, TextInput, StyleSheet } from "react-native";
+import { AntDesign, MaterialIcons } from "@expo/vector-icons"
+
+const InputBox = () => {
+	const [newMessage, setNewMessage] = useState('');
+
+  const onSend = () => {
+    console.warn("Send a new message: ", newMessage);
+  };
+
+  return (
+    <View style={styles.container}>
+      <AntDesign name='plus' size={24} color='royalblue' />
+      <TextInput
+        value={newMessage}
+        onChangeText={setNewMessage}
+        placeholder={"Type a message"}
+        style={styles.input}
+      />
+      <MaterialIcons onPress={onSend} style={styles.send} name='send' size={24} color="white" />
+    </View>
+  )
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    backgroundColor: "whitesmoke",
+    padding: 5,
+    alignItems: "center",
+  },
+  input: {
+    fontSize: 18,
+
+    flex: 1,
+    backgroundColor: "white",
+    padding: 5,
+    paddingHorizontal: 10,
+    marginHorizontal: 10,
+
+    borderRadius: 50,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: "lightgray",
+  },
+  send: {
+    backgroundColor: "royalblue",
+    padding: 7,
+    borderRadius: 15,
+    overflow: "hidden",
+  },
+});
+
+export default InputBox
--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,19 +1,27 @@
-import { View, Text, ImageBackground, StyleSheet, FlatList } from "react-native";
+import { ImageBackground, StyleSheet, FlatList, KeyboardAvoidingView } from "react-native";
 import Message from "../components/Message";
 import bg from "../../assets/images/BG.png"
 import messages from "../../assets/data/messages.json"
+import InputBox from "../components/InputBox";
 
 
 const ChatScreen = () => {
   return (
-    <ImageBackground source={bg} style={styles.bg}>
-      <FlatList 
-        data={messages}
-        renderItem={({ item }) => <Message message={item}/>}
-        style={styles.list}
-        inverted
-      />
-    </ImageBackground>
+    <KeyboardAvoidingView
+    behavior={Platform.OS === "ios" ? "padding" : "height"}
+    style={styles.bg}
+    >
+      <ImageBackground source={bg} style={styles.bg}>
+        <FlatList 
+          data={messages}
+          renderItem={({ item }) => <Message message={item}/>}
+          style={styles.list}
+          inverted
+        />
+
+        <InputBox />
+      </ImageBackground>
+    </KeyboardAvoidingView>
   );
 };
 
