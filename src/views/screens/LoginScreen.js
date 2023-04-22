cd5d6bfc7ba2179646d0191b1e322e7aee46c25a
Ezzat Abdallah <20049524@stu.mmu.ac.uk>
2023-04-22 14:49:16 +0100

    Made login functional with API

--- src/views/screens/LoginScreen.js
+++ src/views/screens/LoginScreen.js
@@ -1,23 +1,38 @@
 import React, { useState } from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-} from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import InputField from '../components/InputField';
+import emailValidator from 'email-validator';
+import ApiWrapper from '../../api/ApiWrapper';
 
 const LoginScreen = () => {
+  const [apiResponce, setApiResponce] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [submitted, setSubmitted] = useState('');
 
   const navigation = useNavigation();
 
-  const handleLogin = () => {
-    navigation.navigate('MainApp');
+  const handleLogin = async () => {
+    if (validateEmail(email)) {
+      setApiResponce('Must enter a valid email.');
+      return;
+    }
+
+    setSubmitted(true);
+    const responce = await ApiWrapper.login(email, password);
+    if (responce.status == 200) navigation.navigate('MainApp');
+    if (responce.status == 400)
+      setApiResponce('Email and password do not match.');
+    if (responce.status == 500)
+      setApiResponce('What the fuck happend here then.');
+    setSubmitted(false);
+  };
+
+  const validateEmail = (email) => {
+    if (!emailValidator.validate(email)) return 'Must enter a valid email.';
+    return null;
   };
 
   return (
@@ -28,6 +43,7 @@ const LoginScreen = () => {
       <InputField
         value={email}
         onChangeText={setEmail}
+        errorMessage={validateEmail(email)}
         placeholder="Email Adress"
       />
       {/* Password */}
@@ -36,10 +52,24 @@ const LoginScreen = () => {
         onChangeText={setPassword}
         placeholder="Password"
       />
-      <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+
+      {/* Login Button */}
+      <TouchableOpacity
+        style={styles.loginButton}
+        onPress={handleLogin}
+        disabled={submitted}
+      >
         <Text style={styles.loginText}>LOG IN</Text>
       </TouchableOpacity>
-      <Text>Dont have an account?</Text>
+
+      {/* Error Message */}
+      {apiResponce ? (
+        <Text style={styles.responceText}>{apiResponce}</Text>
+      ) : (
+        <Text>Dont have an account?</Text>
+      )}
+
+      {/* Signup Button */}
       <TouchableOpacity
         style={styles.signupButton}
         onPress={() => navigation.navigate('SignUp')}
@@ -62,21 +92,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 20,
   },
-  inputContainer: {
-    width: '80%',
-    marginBottom: 10,
-  },
-  inputLabel: {
-    fontSize: 14,
-    marginBottom: 10,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    padding: 10,
-    fontSize: 16,
-  },
   loginButton: {
     backgroundColor: '#25D366',
     padding: 10,
@@ -98,6 +113,9 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  responceText: {
+    color: 'red',
   },
 });
 
